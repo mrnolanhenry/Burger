2a8b9bf4e5d052adcdffb92ea4f24afdbf209a06
enfenry <enfenry@vt.edu>
2019-03-27 14:33:14 -0400

    styling

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,4 +1,8 @@
-INSERT INTO burgers(burger_name,devoured) VALUES ("Bacon burger",false);
+INSERT INTO burgers(burger_name,devoured) VALUES ("Good burger",false);
 INSERT INTO burgers(burger_name,devoured) VALUES ("Avocado burger",false);
 INSERT INTO burgers(burger_name,devoured) VALUES ("Veggie burger",false);
+INSERT INTO burgers(burger_name,devoured) VALUES ("Black bean burger",false);
+INSERT INTO burgers(burger_name,devoured) VALUES ("Edamame burger",false);
 INSERT INTO burgers(burger_name,devoured) VALUES ("Turkey burger",false);
+INSERT INTO burgers(burger_name,devoured) VALUES ("Tofurkey burger",false);
+INSERT INTO burgers(burger_name,devoured) VALUES ("Jerky burger",false);
